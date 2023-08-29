132a782f957b7276d332ca631a74e23c43b870ef
Kevin Brown <kbmp82@gmail.com>
2023-08-29 12:04:35 -0700

    rename docs back to dist

--- webpack.config.js
+++ webpack.config.js
@@ -80,7 +80,7 @@ if (currentTask == "webpackBuild") {
   config.mode = "production";
   config.output = {
     publicPath: "/",
-    path: path.resolve(__dirname, "docs"),
+    path: path.resolve(__dirname, "dist"),
     filename: "[name].[chunkhash].js",
     chunkFilename: "[name].[chunkhash].js",
   };
