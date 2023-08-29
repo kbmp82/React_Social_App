bad05ed1d0674c92af38c2497bd37335e1f79fe0
Kevin Brown <kbmp82@gmail.com>
2023-08-29 11:34:10 -0700

    remove dist and update webpack to create docs folder for github pages

--- webpack.config.js
+++ webpack.config.js
@@ -80,7 +80,7 @@ if (currentTask == "webpackBuild") {
   config.mode = "production";
   config.output = {
     publicPath: "/",
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "docs"),
     filename: "[name].[chunkhash].js",
     chunkFilename: "[name].[chunkhash].js",
   };
